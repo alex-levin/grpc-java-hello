426cebe3a57a304457aa5516833bf20d567cddea
alex-levin <alexlevinb@gmail.com>
2021-09-18 18:05:21 -0400

    first commit

---
+++ src/main/java/io/alevin/grpc/GrpcClient.java
@@ -0,0 +1,24 @@
+package io.alevin.grpc;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class GrpcClient {
+    public static void main(String[] args) {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080)
+          .usePlaintext()
+          .build();
+
+        HelloServiceGrpc.HelloServiceBlockingStub stub 
+          = HelloServiceGrpc.newBlockingStub(channel);
+
+        HelloResponse helloResponse = stub.hello(HelloRequest.newBuilder()
+          .setFirstName("Google")
+          .setLastName("gRPC")
+          .build());
+        
+        System.out.println(helloResponse.getGreeting());
+
+        channel.shutdown();
+    }
+}
---
+++ src/main/java/io/alevin/grpc/HelloServiceImpl.java
@@ -0,0 +1,26 @@
+package io.alevin.grpc;
+
+import io.alevin.grpc.HelloServiceGrpc.HelloServiceImplBase;
+import io.grpc.stub.StreamObserver;
+
+public class HelloServiceImpl extends HelloServiceImplBase {
+
+    @Override
+    public void hello(
+      HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+
+        String greeting = new StringBuilder()
+          .append("Hello, ")
+          .append(request.getFirstName())
+          .append(" ")
+          .append(request.getLastName())
+          .toString();
+
+        HelloResponse response = HelloResponse.newBuilder()
+          .setGreeting(greeting)
+          .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
